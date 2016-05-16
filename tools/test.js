022f0996950157165d84cda72845e3a11d6baba2
kotborealis <kotborealis@gmail.com>
2016-05-16 13:06:22 +0300

    updated test tool

--- tools/test.js
+++ tools/test.js
@@ -22,20 +22,20 @@ fs.readdirSync(test_dir).map(file=>{
     }
 });
 
-tests_in.sort((a,b)=>a.slice(4,-3)*1>b.slice(4,-3)*1).map(test=>exec(`${bin} < ${test_dir}`+test,(code,stdout,stderr)=>{
-    var out = stdout.split("\n").slice(0,-1);
-    var r = {
-        name:test,
-        time:out.length>=2?Number.parseFloat(out[0].slice(0,-2)):-1,
-        passed:out.length>=2?out[1]===tests_out[test]:out[0]===tests_out[test]
-    };
-    var output="";
-    output+=r.passed?"\033[32m":"\033[31m";
-    output+=r.passed?"OK":"ERROR";
-    output+=` ${r.name} `;
-    output+=` [${r.time}]`;
-    output+="\033[0m";
-    console.log(output);
-}));
-
-
+tests_in.sort((a,b)=>a.slice(4,-3)*1>b.slice(4,-3)*1).map(test=>{
+    exec(`${bin} < ${test_dir}/${test}`,(code,stdout,stderr)=>{
+        var out = stdout.split("\n").slice(0,-1);
+        var r = {
+            name:test,
+            time:out.length>=2?Number.parseFloat(out[0].slice(0,-2)):-1,
+            passed:out.length>=2?out[1]===tests_out[test]:out[0]===tests_out[test]
+        };
+        var output="";
+        output+=r.passed?"\033[32m":"\033[31m";
+        output+=r.passed?"OK":"ERROR";
+        output+=` ${r.name} `;
+        output+=` [${r.time}]`;
+        output+="\033[0m";
+        console.log(output);
+    })
+});
